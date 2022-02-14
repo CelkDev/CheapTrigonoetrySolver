2e577439e4b5090b62f7c14ea6bfee5fe90d4192
CelkDev <charleslkappos@outlook.com>
2022-02-13 19:55:42 -0500

    Formating cause atom was having a stroke because my lines were longer than 80 characters.

--- Main.py
+++ Main.py
@@ -3,35 +3,42 @@
 import re
 import numpy as np
 
+
 def reverseCosCalc(a, b, c):
     Angle = np.arccos((a**2-b**2-c**2)/(0-(2*b*c)))
     return Angle
 
-def reverseCos(a,b,c):
+
+def reverseCos(a, b, c):
     angles = []
-    angles.insert(0, (reverseCosCalc(a,b,c)))
-    angles.insert(1, (reverseCosCalc(b,c,a)))
-    angles.insert(2, (reverseCosCalc(c,a,b)))
+    angles.insert(0, (reverseCosCalc(a, b, c)))
+    angles.insert(1, (reverseCosCalc(b, c, a)))
+    angles.insert(2, (reverseCosCalc(c, a, b)))
     return angles
 
-def pythagCalc(a,b,):
+
+def pythagCalc(a, b,):
     return np.sqrt(a**2+b**2)
 
-def pythagManager(a,b,c):
+
+def pythagManager(a, b, c):
     side1 = pythagCalc(abs(a[0]-b[0]), abs(a[1]-b[1]))
     side2 = pythagCalc(abs(b[0]-c[0]), abs(b[1]-c[1]))
     side3 = pythagCalc(abs(c[0]-a[0]), abs(c[1]-a[1]))
     return [side1, side2, side3]
 
-#-----------------------------------------------------------------------------------------------------------------------------------------------------#
-# Have fun figuring this shit out later mate. No comments for you you fuckin idiot.                                                                   #
-#-----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------#
+# Have fun figuring this shit out later mate.
+# No comments for you you fuckin idiot.
+# -----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 if (__name__ == "__main__"):
-    # objPoints = input("What are the three points? Format as (x1,y1)-(x2,y2)-(x3,y3): ")
-    objPoints = "(0, 3 )-(0,0)-(4 ,0)"
+    objPoints = "(23.7, 2)-(19.17, 7)-(11.11, 13)"
     objPoints = objPoints.split("-")
-    dataVars =[list(map(float, (re.sub("\(|\)|\ ", "", objPoints[x]).split(",")))) for x in range(len(objPoints))] #Jesus fucking christ what the fuck did i just make?
+    dataVars = [list(map(float, (re.sub("\(|\)|\ ", "", objPoints[x]).split(",")))) for x in range(len(objPoints))]  # Jesus fucking christ what the fuck did i just make? # noqa
     locationData = dataVars
-    dataVars = pythagManager(dataVars[0], dataVars[1], dataVars[2]) 
-    dataVars = [locationData, dataVars, (reverseCos(dataVars[0], dataVars[1], dataVars[2]))]
+    dataVars = pythagManager(dataVars[0], dataVars[1], dataVars[2])
+    dataVars = [locationData, dataVars, (reverseCos(dataVars[0], dataVars[1], dataVars[2]))] # noqa
+    print(dataVars)
